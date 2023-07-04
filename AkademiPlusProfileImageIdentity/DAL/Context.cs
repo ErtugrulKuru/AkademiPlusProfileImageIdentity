8cc89b213b00907677a69e0956ae3953228bf61a
Hamza Ertugrul Kuru <hertugrulkuru@gmail.com>
2023-07-04 21:19:19 +0300

    Add project files.

---
+++ AkademiPlusProfileImageIdentity/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace AkademiPlusProfileImageIdentity.DAL
+{
+    public class Context : IdentityDbContext<AppUser, AppRole, int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-JH4NS85;initial catalog=AkademiPlusDbImageProfile;integrated Security=true");
+        }
+    }
+}
